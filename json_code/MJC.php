2d734ed92b223c18198ffa207ab76bee212eec8b
Mohamed Abdulalim <megyptm@gmail.com>
2024-01-12 21:35:47 +0200

    1.0.1

--- json_code/MJC.php
+++ json_code/MJC.php
@@ -10,9 +10,30 @@
 namespace Maatify\JsonCode;
 
 use Maatify\Functions\GeneralFunctions;
+use \App\Assist\MJCVarCodes;
 
 class MJC extends JsonGeneralResponse
 {
+    protected static int|string $line;
+    private static self $instance;
+
+    public static function obj(): self
+    {
+        if (! isset(self::$instance)) {
+            self::$instance = new self();
+        }
+        self::$line = debug_backtrace()[0]['line'];
+
+        return self::$instance;
+    }
+
+    protected static MJCVarCodes $json_var_codes;
+
+    public function __construct()
+    {
+        self::$json_var_codes = MJCVarCodes::obj();
+    }
+
     public static function Missing(
         string $varName,
         string $moreInfo = '',
@@ -31,7 +52,7 @@ class MJC extends JsonGeneralResponse
         self::PostErrorHandler($varName, 2000, $moreInfo, $line);
     }
 
-    public static function Exist(string $varName, string $moreInfo = '',int|string $line = ''): void
+    public static function Exist(string $varName, string $moreInfo = '', int|string $line = ''): void
     {
         self::PostErrorHandler($varName, 3000, $moreInfo, $line);
     }
@@ -63,12 +84,12 @@ class MJC extends JsonGeneralResponse
     public static function Success(array $result = [], string $description = '', string $more_info = '', int|string $line = ''): void
     {
         self::HeaderResponseJson([
-            'success'     => true,
-            'response'    => 200,
-            'result' => $result,
-            'description' => $description,
-            'more_info'   => $more_info,
-            'error_details'   => GeneralFunctions::CurrentPageError($line? : debug_backtrace()[0]['line']),
+            'success'       => true,
+            'response'      => 200,
+            'result'        => $result,
+            'description'   => $description,
+            'more_info'     => $more_info,
+            'error_details' => GeneralFunctions::CurrentPageError($line ? : debug_backtrace()[0]['line']),
         ]);
     }
 
@@ -80,19 +101,74 @@ class MJC extends JsonGeneralResponse
     ): void
     {
         self::ErrorWithHeader400(
-            $CodeRange,
+            self::InputCode($varName) + $CodeRange,
             $varName,
             self::ErrorDescription($varName, $CodeRange),
             $moreInfo,
             $line
         );
+    }
 
+    private static function Codes(string $varName): int
+    {
+        return match ($varName) {
+            'action' => 10,
+            'api' => 11,
+            'app_type' => 12,
+            'main_hash' => 13,
+            'hash' => 14,
+            'token' => 15,
+            'device_id' => 16,
+            'device_name' => 17,
+            'device_version' => 18,
+            'account_no' => 19,
+            'phone' => 20,
+            'email' => 21,
+            'phone_or_email' => 22,
+            'username' => 23,
+            'code' => 24,
+            'password' => 25,
+            'old_password' => 26,
+            'transaction_pin' => 27,
+            'old_transaction_pin' => 28,
+            'ip' => 29,
+            'language' => 30,
+            'national_id' => 31,
+            'credentials' => 32,
+            'first_name' => 33,
+            'last_name' => 34,
+            'name' => 35,
+            'description' => 36,
+            'status' => 37,
+            'title' => 38,
+            'birthdate' => 39,
+            'gender' => 40,
+            'country' => 41,
+            'governorate' => 42,
+            'city' => 43,
+            'area' => 44,
+            'address' => 45,
+            'info' => 46,
+            'base64_file' => 47,
+            'file' => 48,
+            default => 0,
+        };
+    }
+
+    private static function InputCode(string $varName): int
+    {
+        if (! $code = self::$json_var_codes::Codes($varName)) {
+            $code = self::Codes($varName);
+        }
+
+        return $code;
     }
 
     private static function ErrorDescription(
         string $varName,
         int $CodeRange
-    ): string {
+    ): string
+    {
         return str_replace(
             '??',
             ucwords(str_replace('_', ' ', $varName), ' '),
@@ -108,7 +184,8 @@ class MJC extends JsonGeneralResponse
             }
         );
     }
-    public static function DbError(int|string  $line = 0): void
+
+    public static function DbError(int|string $line = 0): void
     {
         http_response_code(500);
         self::HeaderResponseJson([
